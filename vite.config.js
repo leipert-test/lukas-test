a75928567e5d1fed81c6d8b88e142b130156699f
Lukas Eipert <2906107+leipert@users.noreply.github.com>
2025-08-20 14:33:45 +0200

    Multiline syntax highlighting

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,21 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+/**
+ *
+ * very long multiline comment
+ *
+ *
+*/
+
+const foo = `
+very
+long
+multi
+line
+string
+`;
+
 export default defineConfig({
   plugins: [react()],
 })
