5f4cf8f9b11d45a6a0684a4bd284f37711397bcc
Lukas Eipert <lukas@linear.app>
2026-03-19 11:44:35 +0100

    Add kebabCase converter to string utils

--- src/utils/string.js
+++ src/utils/string.js
@@ -10,3 +10,10 @@ export function camelCase(str) {
     .replace(/^./, (c) => c.toLowerCase());
 }
 
+export function kebabCase(str) {
+  return str
+    .replace(/([a-z])([A-Z])/g, '$1-$2')
+    .replace(/[\s_]+/g, '-')
+    .toLowerCase();
+}
+
