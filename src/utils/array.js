7b61c9873d89034908a2b1110164ae86e8baee43
Lukas Eipert <lukas@linear.app>
2026-03-19 11:45:11 +0100

    Add difference function to array utils

--- src/utils/array.js
+++ src/utils/array.js
@@ -23,3 +23,8 @@ export function intersection(a, b) {
   return a.filter((x) => setB.has(x));
 }
 
+export function difference(a, b) {
+  const setB = new Set(b);
+  return a.filter((x) => !setB.has(x));
+}
+
