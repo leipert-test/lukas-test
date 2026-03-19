7ccacecef1020d895b0b1a6bf867b4b3dd73aa30
Lukas Eipert <lukas@linear.app>
2026-03-19 11:45:04 +0100

    Add chunk function to array utils

---
+++ src/utils/array.js
@@ -0,0 +1,10 @@
+// Array utility functions
+
+export function chunk(arr, size) {
+  const result = [];
+  for (let i = 0; i < arr.length; i += size) {
+    result.push(arr.slice(i, i + size));
+  }
+  return result;
+}
+
