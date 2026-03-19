f196d6b12a823d7cbe8b1316df5c96c1541f24d7
Lukas Eipert <lukas@linear.app>
2026-03-19 11:44:39 +0100

    Add truncate function to string utils

--- src/utils/string.js
+++ src/utils/string.js
@@ -24,3 +24,8 @@ export function snakeCase(str) {
     .toLowerCase();
 }
 
+export function truncate(str, length, suffix = '...') {
+  if (str.length <= length) return str;
+  return str.slice(0, length - suffix.length) + suffix;
+}
+
