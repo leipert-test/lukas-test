c553e9f83cf3aacd8bf50ceb3cf84c27ddc071fe
Lukas Eipert <lukas@linear.app>
2026-03-19 11:44:48 +0100

    Add palindrome checker to string utils

--- src/utils/string.js
+++ src/utils/string.js
@@ -56,3 +56,8 @@ export function isEmail(str) {
   return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(str);
 }
 
+export function isPalindrome(str) {
+  const cleaned = str.toLowerCase().replace(/[^a-z0-9]/g, '');
+  return cleaned === [...cleaned].reverse().join('');
+}
+
