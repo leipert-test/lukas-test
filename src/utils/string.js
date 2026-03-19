0cf4076a258457faa6840cbbdd6f8c8a16fbabc0
Lukas Eipert <lukas@linear.app>
2026-03-19 11:47:20 +0100

    Add initials extractor to string utils

--- src/utils/string.js
+++ src/utils/string.js
@@ -104,3 +104,7 @@ export function wrap(str, wrapper) {
   return wrapper + str + wrapper;
 }
 
+export function initials(str) {
+  return str.split(/\s+/).map((w) => w.charAt(0).toUpperCase()).join('');
+}
+
