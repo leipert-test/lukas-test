81f7bdbc29ccb952d480c404a002de559d8c899c
Lukas Eipert <lukas@linear.app>
2026-03-19 11:44:34 +0100

    Add camelCase converter to string utils

--- src/utils/string.js
+++ src/utils/string.js
@@ -4,3 +4,9 @@ export function capitalize(str) {
   return str.charAt(0).toUpperCase() + str.slice(1);
 }
 
+export function camelCase(str) {
+  return str
+    .replace(/[^a-zA-Z0-9]+(.)/g, (_, c) => c.toUpperCase())
+    .replace(/^./, (c) => c.toLowerCase());
+}
+
