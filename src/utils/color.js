58c2e86e1e2080d1b6a1123d95ed23e676d04102
Lukas Eipert <lukas@linear.app>
2026-03-19 11:45:48 +0100

    Add RGB to HSL converter to color utils

--- src/utils/color.js
+++ src/utils/color.js
@@ -42,3 +42,21 @@ export function complementary(hex) {
   return rgbToHex(255 - r, 255 - g, 255 - b);
 }
 
+export function rgbToHsl(r, g, b) {
+  r /= 255; g /= 255; b /= 255;
+  const max = Math.max(r, g, b), min = Math.min(r, g, b);
+  let h, s, l = (max + min) / 2;
+  if (max === min) {
+    h = s = 0;
+  } else {
+    const d = max - min;
+    s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+    switch (max) {
+      case r: h = ((g - b) / d + (g < b ? 6 : 0)) / 6; break;
+      case g: h = ((b - r) / d + 2) / 6; break;
+      case b: h = ((r - g) / d + 4) / 6; break;
+    }
+  }
+  return { h: Math.round(h * 360), s: Math.round(s * 100), l: Math.round(l * 100) };
+}
+
