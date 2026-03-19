7ccfec790d58ee5816eb55b662399282747867fa
Lukas Eipert <lukas@linear.app>
2026-03-19 11:46:25 +0100

    Add barrel export for all utility modules

---
+++ src/utils/index.js
@@ -0,0 +1,6 @@
+// Barrel exports for all utility modules
+export * as math from './math.js';
+export * as string from './string.js';
+export * as array from './array.js';
+export * as color from './color.js';
+export * as date from './date.js';
